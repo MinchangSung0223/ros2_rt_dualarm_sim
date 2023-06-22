a2a16ffdff28091843391d2bc2e30a1c972d4702
Minchang Sung <tjdalsckd@gmail.com>
2023-06-22 22:35:26 +0900

    20230622_223526

--- git_push.sh
+++ git_push.sh
@@ -4,6 +4,6 @@ now_date=$(date +"%Y%m%d_%H%M%S")
 
 git add *
 git commit -m "$now"
-git remote add "$now_date" git@github.com:SHKim-HYU/RTIndy7.git
-git push "$now_date" dualarm
+git remote add "$now_date" git@github.com:MinchangSung0223/ros2_rt_dualarm.git
+git push "$now_date" main
 
